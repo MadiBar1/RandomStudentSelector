2189df57453de2781000f46607ac1e5c90738992
Madison Harris <madison.harris002@student.swic.edu>
2025-11-17 20:53:01 -0600

    Fix dark mode and light mode, Feat text change color with radio selector

--- src/main.js
+++ src/main.js
@@ -4,18 +4,13 @@ const body = document.body;
 const darkModeRadio = document.getElementById("dark");
 const lightModeRadio = document.getElementById("light");
 
-document.querySelector("#app").innerHTML = `
-  <h1 class="text-3xl font-bold underline">
-    Hello Vite!
-  </h1>
-`;
-
 darkModeRadio.addEventListener("change", () => {
-  body.classList.remove("bg-gray-400");
-  body.classList.add("bg-gray-800");
+  console.log("dark mode selected");
+  body.classList.remove("bg-gray-200", "text-black");
+  body.classList.add("bg-gray-800", "text-white");
 });
 
 lightModeRadio.addEventListener("change", () => {
-  body.classList.remove("bg-gray-800");
-  body.classList.add("bg-gray-400");
+  body.classList.remove("bg-gray-800", "text-white");
+  body.classList.add("bg-gray-200", "text-black");
 });
