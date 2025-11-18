5d7f5aede8870502cb350c18bf88c845f87d9da5
Madison Harris <madison.harris002@student.swic.edu>
2025-11-17 20:36:35 -0600

    feat darkmode and lightmode

--- src/main.js
+++ src/main.js
@@ -1,7 +1,21 @@
 import "./style.css";
 
+const body = document.body;
+const darkModeRadio = document.getElementById("dark");
+const lightModeRadio = document.getElementById("light");
+
 document.querySelector("#app").innerHTML = `
   <h1 class="text-3xl font-bold underline">
     Hello Vite!
   </h1>
 `;
+
+darkModeRadio.addEventListener("change", () => {
+  body.classList.remove("bg-gray-400");
+  body.classList.add("bg-gray-800");
+});
+
+lightModeRadio.addEventListener("change", () => {
+  body.classList.remove("bg-gray-800");
+  body.classList.add("bg-gray-400");
+});
