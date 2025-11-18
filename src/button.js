367150cda5dfc5f2e99c40ce8fd73f23a15c68ee
Madison Harris <madison.harris002@student.swic.edu>
2025-11-17 20:09:21 -0600

    add button event to select random student

--- src/button.js
+++ src/button.js
@@ -1,3 +1,13 @@
+import "./style.css";
+import getStudents from "./students.js";
+
 const button = document.getElementById("randomStudentButton");
 
-
+button.addEventListener("click", () => {
+  const students = getStudents();
+  const randomIndex = Math.floor(Math.random() * students.length);
+  const randomId = students[randomIndex].ID;
+  const selectedStudent = students.find((student) => student.ID === randomId);
+  const displayElement = document.getElementById("selectedStudent");
+  displayElement.textContent = `Selected Student: ${selectedStudent.Name}`;
+});
