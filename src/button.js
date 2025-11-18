791387fdf75727037c5f5695cfd7df38199f5e4a
Madison Harris <madison.harris002@student.swic.edu>
2025-11-17 20:19:47 -0600

    refractor simplified button function

--- src/button.js
+++ src/button.js
@@ -4,10 +4,11 @@ import getStudents from "./students.js";
 const button = document.getElementById("randomStudentButton");
 
 button.addEventListener("click", () => {
-  const students = getStudents();
-  const randomIndex = Math.floor(Math.random() * students.length);
-  const randomId = students[randomIndex].ID;
-  const selectedStudent = students.find((student) => student.ID === randomId);
-  const displayElement = document.getElementById("selectedStudent");
-  displayElement.textContent = `Selected Student: ${selectedStudent.Name}`;
+ const randomStudent = 
 });
+
+function getRandomIntInclusive(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
+}
